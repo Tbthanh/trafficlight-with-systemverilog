6561a4741e5de7cc6c79d4644ce7f1570c18cd44
Tbthanh <thanhtb2003ltv@gmail.com>
2024-04-27 11:19:03 +0700

    update 03: added condition to timer
    added comb to update when to count down from green, yellow. added ff to store value of count_g and count_y to count.
    
    Not sure its correctness but lets moved on to the topmodule.

--- timer.sv
+++ timer.sv
@@ -11,18 +11,42 @@ module timer #( parameter count_g 	= 26'd249_999_999,	// 5s
 	output timeout
 );
 	reg [26:0] count;
+	reg count_to;	// to decicde which count_x
+	
+	always_comb begin : proc_counto
+		if (~rst_n) begin
+			count_to = 1;
+		end else (start) begin
+			count_to = count_to + 1;
+		end
+	end
 
-	// reg load;
-	// assign load = (start & (enable_h | enable_n));
-
+	always_ff @(start) begin : proc_assign_count_value
+		if(count_to) begin
+			 count <= count_g;
+		end else begin
+			 count <= count_y;
+		end
+	end
 
 	always @(posedge clk or negedge rst_n) begin
 	 	if(~rst_n) begin
-	 		count <= count_g;
 	 		Timeout <= 0;
 	 		timeout <= 0;
 	 	end else begin
-	 		 
+	 		if (count != 0) begin
+	 			count = count - 1;
+	 		end else begin
+	 			case (count_to)
+	 				1'b0 : timeout <= 1;
+	 				1'b1 : Timeout <= 1;
+	 				default : begin
+	 					timeout = 0;
+	 					Timeout = 0;
+	 				end
+	 			endcase
+	 		end
+
 	 	end
 	 end
 
---
+++ trafficlight.sv
@@ -0,0 +1,10 @@
+module trafficlight (
+	input clk,    // Clock
+	input rst_n,  // Asynchronous reset active low
+	input car,
+
+	output light
+);
+
+	
+endmodule : trafficlight
